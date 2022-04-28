1d28b73e49580ccc9d5e395e3e1e7e9280e8a1f4
Michael Katz <mikekatz04@gmail.com>
2022-04-28 12:22:26 +0200

    tests updated for information matrix

--- gbgpu/tests/test_gbgpu.py
+++ gbgpu/tests/test_gbgpu.py
@@ -77,7 +77,7 @@ class WaveformTest(unittest.TestCase):
         Tobs = 4.0 * YEAR
         gb = GBGPU(use_gpu=gpu_available)
         gb.d_d = 0.0
-        
+
         N = int(256)
         num_bin = 10
         amp = 1e-22  # amplitude
@@ -147,3 +147,60 @@ class WaveformTest(unittest.TestCase):
         )
 
         like = gb.get_ll(params, data, noise_factor, N=N, dt=dt, T=Tobs,)
+
+        self.assertFalse(xp.any(xp.isnan(like)))
+
+    def test_information_matrix(self):
+
+        dt = 15.0
+        Tobs = 4.0 * YEAR
+        gb = GBGPU(use_gpu=gpu_available)
+        gb.d_d = 0.0
+
+        N = int(256)
+        num_bin = 10
+        amp = 1e-22  # amplitude
+        f0 = 2e-3  # f0
+        fdot = 1e-14  # fdot
+        fddot = 0.0
+        phi0 = 0.1  # initial phase
+        iota = 0.2  # inclination
+        psi = 0.3  # polarization angle
+        lam = 0.4  # ecliptic longitude
+        beta_sky = 0.5  # ecliptic latitude
+
+        amp_in = np.full(num_bin, amp)
+        f0_in = np.full(num_bin, f0)
+        fdot_in = np.full(num_bin, fdot)
+        fddot_in = np.full(num_bin, fddot)
+        phi0_in = np.full(num_bin, phi0)
+        iota_in = np.full(num_bin, iota)
+        psi_in = np.full(num_bin, psi)
+        lam_in = np.full(num_bin, lam)
+        beta_sky_in = np.full(num_bin, beta_sky)
+
+        length = int(Tobs / dt)
+
+        freqs = np.fft.rfftfreq(length, dt)
+        data_stream_length = len(freqs)
+
+        params = np.array(
+            [amp_in, f0_in, fdot_in, fddot_in, phi0_in, iota_in, psi_in, lam_in, beta_sky_in,]
+        )
+
+        inds = np.array([0, 1, 2, 4, 5, 6, 7, 8])
+
+        info_matrix = gb.information_matrix(
+            params,
+            easy_central_difference=False,
+            eps=1e-9,
+            inds=inds,
+            N=1024,
+            dt=dt,
+            T=Tobs,
+        )
+
+        cov = np.linalg.pinv(info_matrix)
+        self.assertFalse(xp.any(xp.isnan(cov)))
+
+
