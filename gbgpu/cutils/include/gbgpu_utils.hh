93dc7a690bf5c90454dec56137de9ae71f24c1e7
Michael Katz <mikekatz04@gmail.com>
2025-05-11 18:44:59 +0200

    along rebase 6

--- gbgpu/cutils/include/gbgpu_utils.hh
+++ gbgpu/cutils/include/gbgpu_utils.hh
@@ -12,9 +12,20 @@ void get_ll_wrap(cmplx *d_h, cmplx *h_h,
                  double *A_psd, double *E_psd, double df,
                  int *start_ind, int M, int num_bin, int *data_index, int *noise_index, int data_length);
 
-void direct_like(cmplx *d_h, cmplx *h_h,
-                 cmplx *A_template, cmplx *E_template,
-                 cmplx *A_data, cmplx *E_data,
-                 int data_length, int start_freq_ind, int nwalkers);
+void get_ll_wrap(cmplx* d_h, cmplx* h_h,
+                  cmplx* A_template, cmplx* E_template,
+                  cmplx* A_data, cmplx* E_data,
+                  double* A_psd, double* E_psd, double df,
+                  int* start_ind, int M, int num_bin, int* data_index, int* noise_index, int data_length);
+
+void direct_like(cmplx* d_h, cmplx* h_h,
+               cmplx* A_template, cmplx* E_template,
+               cmplx* A_data, cmplx* E_data,
+               int data_length, int start_freq_ind, int nwalkers);
+
+void set_threads(int num_threads);
+int get_threads();
+
+void swap_ll_diff_wrap(cmplx* d_h_remove, cmplx* d_h_add, cmplx* add_remove, cmplx* remove_remove, cmplx* add_add, cmplx* A_remove, cmplx* E_remove, int* start_ind_all_remove, cmplx* A_add, cmplx* E_add, int* start_ind_all_add, cmplx* A_data, cmplx* E_data, double* A_psd, double* E_psd, double df, int M, int num_bin, int* data_index, int* noise_index, int data_length);
 
 #endif // __NEW_FASTGB_HH__
